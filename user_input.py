bff7236f08e94e270ed5aaedcb73886b57b6b334
Okemwa Brian <114869400+okemwabrian@users.noreply.github.com>
2024-08-16 17:01:53 +0300

    Add files via upload
    Add user input program

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Asking the user for their name
+name = input("What is your name? ")
+
+# Asking the user for their age
+age = input("How old are you? ")
+
+# Asking the user for their location
+location = input("Where do you live? ")
+
+# Print 
+print(f"Hello {name}, you are {age} years old and live in {location}.")
